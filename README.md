8dd0a4559777883d135acb538a392fe776f99b56
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-08-07 21:58:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# JS_todo
+ JS todo
---
+++ js/clock.js
@@ -0,0 +1,23 @@
+const clockContainer = document.querySelector('.js-clock');
+const dayTitle = clockContainer.querySelector('h2');
+const clockTitle = clockContainer.querySelector('h1');
+
+function getTime() {
+	const date = new Date();
+	const month = date.getMonth() + 1;
+	const day = date.getDate();
+	const hours = date.getHours();
+	const minutes = date.getMinutes();
+	const sec = date.getSeconds();
+
+	clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${
+		minutes < 10 ? `0${minutes}` : minutes
+	}:${sec < 10 ? `0${sec}` : sec}`;
+	dayTitle.innerText = `${month}/${day}`;
+}
+
+function init() {
+	setInterval(getTime, 1000);
+}
+
+init();
